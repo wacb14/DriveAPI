631d4de34e7dd282953bd88b942e55b809d3c763
Willy Cruz <wacb14@gmail.com>
2023-08-22 22:25:48 -0500

    Business Service implemented

--- BusinessServices/FileBS.cs
+++ BusinessServices/FileBS.cs
@@ -1,6 +1,25 @@
+using DriveAPI.DataServices;
+
 namespace DriveAPI.BussinesServices
 {
-    public interface IFileBS{
-        
-    }    
+    public class FileBS : IFileBS
+    {
+        private IFileDS _fileDS;
+        public FileBS(IFileDS fileDS)
+        {
+            _fileDS = fileDS;
+        }
+        public Filew GetFile(int id){
+            return _fileDS.GetFile(id);
+        }
+        public Filew PostFile(Filew file){
+            return _fileDS.PostFile(file);
+        }
+        public Filew PutFile(int id, Filew file){
+            return _fileDS.PutFile(id, file);
+        }
+        public int DeleteFile(int id){
+            return _fileDS.DeleteFile(id);
+        }
+    }
 }
--- BusinessServices/Interfaces/IFileBS.cs
+++ BusinessServices/Interfaces/IFileBS.cs
@@ -1,6 +1,11 @@
+using DriveAPI.Models;
+
 namespace DriveAPI.BussinesServices
 {
-    public class FileBS:IFileBS{
-
+    public interface IFileBS{
+        public Filew GetFile(int id);
+        public Filew PostFile(Filew file);
+        public Filew PutFile(int id, Filew file);
+        public int DeleteFile(int id);
     }    
 }
