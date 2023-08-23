9ff73880e5acafc6b17e3a4bed59ea30f3f99136
Willy Cruz <wacb14@gmail.com>
2023-08-22 20:07:20 -0500

    Data services CRUD implemented

--- DataServices/FileDS.cs
+++ DataServices/FileDS.cs
@@ -1,5 +1,44 @@
-namespace DriveAPI.DataServices{
-    public class FileDS:IFileDS{
-                        
+using DriveAPI.Models;
+
+namespace DriveAPI.DataServices
+{
+    public class FileDS : IFileDS
+    {
+        private GeneralContext _generalContext;
+        public FileDS(GeneralContext generalContext)
+        {
+            _generalContext = generalContext;
+        }
+        public Filew GetFile(int id)
+        {
+            return _generalContext.File.Find(id);
+        }
+        public Filew PostFile(Filew file)
+        {
+            _generalContext.File.Add(file);
+            _generalContext.SaveChanges();
+            return _generalContext.File.FirstOrDefault(f => f.id == file.id);
+        }
+        public Filew PutFile(int id, Filew file)
+        {
+            if (file != null)
+            {
+                _generalContext.File.Update(file);
+                _generalContext.SaveChanges();
+            }
+            return _generalContext.File.FirstOrDefault(f => f.id == file.id);
+        }
+        public int DeleteFile(int id){
+            var file = _generalContext.File.FirstOrDefault(f => f.id == id);
+            if (file != null)
+            {
+                _generalContext.File.Remove(file);
+                _generalContext.SaveChanges();
+                return id;
+            }
+            else
+                return -1;
+            
+        }
     }
 }
--- DataServices/Interfaces/IFileDS.cs
+++ DataServices/Interfaces/IFileDS.cs
@@ -1,4 +1,10 @@
+using DriveAPI.Models;
+
 namespace DriveAPI.DataServices{
     public interface IFileDS{
+        public Filew GetFile(int id);
+        public Filew PostFile(Filew file);
+        public Filew PutFile(int id, Filew file);
+        public int DeleteFile(int id);
     }
 }
